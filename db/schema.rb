1448b9a99dfe7efab2289f30b58d93542d98d746
DabooUA <sbardysh@tutanota.com>
2021-06-27 15:39:50 -0400

    creating messages table, inderx.erb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210627152009) do
+ActiveRecord::Schema.define(version: 20210627193121) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "user_id"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20210627152009) do
     t.string   "photos_content_type"
     t.integer  "photos_file_size"
     t.datetime "photos_updated_at"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "user_id"
+    t.string "content"
   end
 
   create_table "users", force: :cascade do |t|
