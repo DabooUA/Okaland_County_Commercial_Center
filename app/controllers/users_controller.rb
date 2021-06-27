86f2f17b65d7b1f0bd7cec42ad37d1aac48ba357
DabooUA <sbardysh@tutanota.com>
2021-06-27 10:08:41 -0400

    creating users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,55 @@
 class UsersController < ApplicationController
 
+    get "/signup" do
+        if !logged_in?
+            erb :'users/signup', locals: {message: "New users need to create account before login in"}
+        else
+            redirect to "/listings"
+        end
+    end
+
+    post '/signup' do
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect to '/signup'
+        else
+            @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+            @user.save
+            session[:user_id] = @user.id
+            redirect to '/listings'
+        end
+    end
+
+    get '/login' do
+        if !logged_in?
+            erb :'users/login'
+        else
+            redirect to '/listings'
+        end
+    end
+
+    post '/login' do
+        @user = User.find_by(:username => params[:username])
+            if @user && @user.authenticate(params[:password])
+                session[:user_id] = @user.id
+                redirect to '/listings'
+            else
+                redirect to '/signup'
+            end
+        
+    end
+
+    get "/logout" do
+        if logged_in?
+            session.clear
+            redirect to "/login"
+        else
+            redirect to "/"
+        end
+    end
+
+    get "/users/:slug" do
+        @user = User.find_by_slug(params[:slug])
+        erb :'users/show'
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
 
     has_many :messages
     has_secure_password
+
+    has_attached_file :pictures, styles: { medium: "400x400>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :pictures, content_type: /\Aimage\/.*\z/
   
     def slug
       username.downcase.gsub(" ","-")
