00d668a490a9a362132e84686dd1080059fba76f
DabooUA <sbardysh@tutanota.com>
2021-07-10 15:08:27 -0400

    Layout for listings, logic to save in DB

--- db/migrate/20210627144228_create_listings.rb
+++ db/migrate/20210627144228_create_listings.rb
@@ -2,12 +2,13 @@ class CreateListings < ActiveRecord::Migration
   def change
     create_table :listings do |l|
       l.string :user_id
-      l.string :content
+      l.string :title
       l.string :year
       l.string :make
       l.string :model
       l.string :miles
       l.string :engine
+      l.string :content
     end
   end
 end
